17c56e9d0f674b3e2d4ae8cdac2a6767f386fe67
yayanan1991 <1539815919@qq.com>
2016-11-25 03:18:17 +0800

    second commit

---
+++ HelloWorld/src/main/java/cn/liyanan/HelloWorld.java
@@ -0,0 +1,24 @@
+package cn.liyanan;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorld extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("doGet Method");
+		PrintWriter pw = resp.getWriter();
+		pw.print("HelloWorld");
+		pw.close();
+	}
+
+
+	
+	
+
+}
